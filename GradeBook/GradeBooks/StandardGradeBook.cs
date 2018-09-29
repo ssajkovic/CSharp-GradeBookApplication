f14c227addc623666e806c87f27a6fe31e8ec0a2
= <stevan.sajkovic@live.com>
2018-09-29 01:56:58 +0100

    "Completed the module"

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name) //call the base constructor upon completion
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
